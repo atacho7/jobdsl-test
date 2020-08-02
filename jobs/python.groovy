26fb7aa5d99350ce9ae7dd2f5f6c14583726e410
Ubuntu <ubuntu@ip-172-31-25-212.us-east-2.compute.internal>
2020-08-02 20:18:20 +0000

    Fichero test.groovy

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -2,6 +2,11 @@ project_name = "jenkins-pipeline-python"
 repo = "https://github.com/atacho7/test-jenkins1.git"
 repo_name = "repo"
 
+folder('project-a') {
+    displayName('Project A')
+    description('Folder for project A')
+}
+
 
 pipelineJob(project_name) {
 	definition {
